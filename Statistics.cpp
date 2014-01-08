7bb3c6b6eb3ccaaabe751a1dbb14edad8998b881
Arnaud Le Rouzic <lerouzic@legs.cnrs-gif.fr>
2014-01-08 14:32:51 +0100

    Multivariate statistics

--- Statistics.cpp
+++ Statistics.cpp
@@ -2,10 +2,11 @@
 
 #include <cassert>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-
+////////////////////////////////// UNIVARIATE ////////////////////////////////////////
 
 // constructors and destructors
 
@@ -49,3 +50,125 @@ double UnivariateStat::var() const {
 	return(sum_i2/static_cast<double>(data.size()) - mm*mm);
 }
 
+
+////////////////////////////////// MULTIVARIATE /////////////////////////////////
+
+MultivariateStat::MultivariateStat(const vector<vector<double> > & v)
+	: data(v)
+{
+	initialize();
+}
+
+MultivariateStat::~MultivariateStat()
+{
+}
+
+void MultivariateStat::initialize()
+{
+	unsigned int size1;
+	
+	// Tests if the data behaves properly (same number of measurements for each category)
+	assert(data.size() > 1);
+	size1 = data[0].size();
+	for (unsigned int i = 1; i < data.size(); i++) { // i = 1: desired behaviour
+		assert(data[i].size() == size1);
+	}
+	
+	// creates the matrix of sum_ij
+	for (unsigned int i = 0; i < data.size(); i++) {
+		vector<double> tmp_sumij(data.size(), 0.0);
+		sum_ij.push_back(tmp_sumij);
+	}
+	
+	for (unsigned int i = 0; i < data.size(); i++) {
+		
+		// vector of sum_i
+		double tmp_sum = 0.0;
+		for (unsigned int k = 0; k < data[i].size(); k++) {
+			tmp_sum += data[i][k];
+		}
+		
+		// matrix of sum_ij
+		for (unsigned int j = i; j < data.size(); j++) {
+			double tmp_sumij = 0.0;
+			for (unsigned int k = 0; k < data[i].size(); k++) {
+				tmp_sumij += data[i][k] * data[j][k];
+			}
+			sum_ij[i][j] = tmp_sum;
+			if (i != j) { // does not harm, but useless when i == j
+				sum_ij[j][i] = tmp_sum; 
+			}
+		}
+	}
+}
+
+vector<double> MultivariateStat::means() const
+{
+	vector<double> ans(data.size());
+	for (unsigned int i = 0; i < data.size(); i++) {
+		ans[i] = sum_i[i]/static_cast<double>(data[i].size());
+	}
+	return(ans);
+}
+
+vector<double> MultivariateStat::vars() const
+{
+	vector<double> ans(data.size());
+	for (unsigned int i = 0; i < data.size(); i++) {
+		ans[i] = var(i);
+	}
+	return(ans);
+}
+
+vector<vector<double> > MultivariateStat::vcov() const
+{
+	vector<vector<double> > ans (data.size());
+	for (unsigned int i = 0; i < data.size(); i++) {
+		vector<double> tmp (data.size());
+		for (unsigned int j = 0; j < data.size(); j++) {
+			tmp[j] = cov(i, j);
+		}
+		ans.push_back(tmp);
+	}
+	return(ans);
+}
+
+double MultivariateStat::mean(unsigned int i) const
+{
+	assert (i < data.size());
+	return(sum_i[i] / static_cast<double>(data[i].size()));
+}
+
+double MultivariateStat::var(unsigned int i) const 
+{
+	assert (i < data.size());
+	return(sum_ij[i][i]/static_cast<double>(data[i].size()) - mean(i)*mean(i));
+}
+
+double MultivariateStat::cov(unsigned int i, unsigned int j) const
+{
+	assert (i < data.size());
+	assert (j < data.size());
+	return(sum_ij[i][j]/static_cast<double>(data[i].size()) - mean(i)*mean(j));
+}
+
+double MultivariateStat::cor(unsigned int i, unsigned int j) const
+{
+	assert (i < data.size());
+	assert (j < data.size());
+	return(cov(i, j)/(sqrt(var(i)*var(j))));
+}
+
+double MultivariateStat::r2(unsigned int i, unsigned int j) const
+{
+	assert (i < data.size());
+	assert (j < data.size());
+	return(cor(i, j)*cor(i, j));	
+}
+
+double MultivariateStat::regression_slope(unsigned int i, unsigned int j) const
+{
+	assert (i < data.size());
+	assert (j < data.size());
+	return(cov(i, j)/var(j));
+}
--- Statistics.h
+++ Statistics.h
@@ -4,7 +4,6 @@
 #include <vector>
 
 
-
 class UnivariateStat
 {
 	public:
@@ -26,7 +25,32 @@ class UnivariateStat
 		double sum_i2;
 };
 
-
+class MultivariateStat
+{
+	public:
+		// constructors/destructors
+		MultivariateStat(const std::vector<std::vector<double> > &);
+		~MultivariateStat();
+		
+		void initialize();
+		
+		// functions
+		std::vector<double> means() const;
+		std::vector<double> vars() const;
+		std::vector<std::vector<double> > vcov() const;
+		
+		double mean(unsigned int) const;
+		double var(unsigned int) const;
+		double cov(unsigned int, unsigned int) const;
+		double cor(unsigned int, unsigned int) const;
+		double r2(unsigned int, unsigned int) const;
+		double regression_slope(unsigned int, unsigned int) const; //param 1 = a*param 2 + b
+		
+	protected:
+		const std::vector<std::vector<double> > data;
+		std::vector<std::vector<double> > sum_ij;
+		std::vector<double> sum_i;
+};
 
 
 
