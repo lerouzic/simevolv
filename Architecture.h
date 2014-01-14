3f497165ad36197e3aee708dc31b2f0d3d29634c
Estelle Runneburger <estelle.runneburger@legs.cnrs-gif.fr>
2014-01-14 15:18:59 +0100

    copyright and header

--- Architecture.h
+++ Architecture.h
@@ -1,3 +1,19 @@
+// Copyright 2004-2007 José Alvarez-Castro <jose.alvarez-castro@lcb.uu.se>
+// Copyright 2007      Arnaud Le Rouzic    <a.p.s.lerouzic@bio.uio.no>
+// Copyright 2014	   Estelle Rünneburger <estelle.runneburger@legs.cnrs-gif.fr>		
+
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
 #ifndef ARCHITECTURE_H_INCLUDED
 #define ARCHITECTURE_H_INCLUDED
 
--- Phenotype.h
+++ Phenotype.h
@@ -1,3 +1,17 @@
+// Copyright 2004-2007 José Alvarez-Castro <jose.alvarez-castro@lcb.uu.se>
+// Copyright 2007      Arnaud Le Rouzic    <a.p.s.lerouzic@bio.uio.no>
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
 #ifndef PHENOTYPE_H_INCLUDED
 #define PHENOTYPE_H_INCLUDED
 
