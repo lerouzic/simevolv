f6b42e288385412a5c17a055856f2af91d5b8fcd
Arnaud Le Rouzic <lerouzic@legs.cnrs-gif.fr>
2014-04-30 14:46:23 +0200

    Comments and cleaning in Fitness.h and Fitness.cpp

--- Fitness.cpp
+++ Fitness.cpp
@@ -1,5 +1,5 @@
 // Copyright 2004-2007 José Alvarez-Castro <jose.alvarez-castro@lcb.uu.se>
-// Copyright 2007      Arnaud Le Rouzic    <a.p.s.lerouzic@bio.uio.no>
+// Copyright 2007-2014 Arnaud Le Rouzic    <a.p.s.lerouzic@bio.uio.no>
 
 /***************************************************************************
  *                                                                         *
@@ -25,11 +25,14 @@
 
 using namespace std;
 
-
+/* Initialization of the instance (static pointer) */
+Fitness * Fitness::instance = NULL;
 
 // constructors and destructor
 
 /* constructor using the parameters from the Pameters files */
+/* the Fitness class keeps a (constant) reference of the parameter set.
+   (Note: what happens if the parameter set is destroyed? this is not particularly safe) */
 Fitness::Fitness(const ParameterSet& param)
     : param(param)
     , type(param.getpar(FITNESS_TYPE)->GetString())
@@ -38,28 +41,31 @@ Fitness::Fitness(const ParameterSet& param)
 {
 }
 
-
-// instance and initialization
-
-/* put the existence of the fitness to non-existent */
-Fitness * Fitness::instance = NULL;
-
-
-/* initialization of the fitness system */
+/* This static function can be used to initialize or reinitialize the Fitness
+   single instance with the parameter set.
+   Note: I am not sure that the reinitialization from the parameter set can be useful
+   in any context. A warning is displayed, but it could be removed if a legitimate use 
+   was discovered. */
 void Fitness::initialize(const ParameterSet& param)
 {
     if (Fitness::instance != NULL)
     {
         delete Fitness::instance;
         Fitness::instance = NULL;
+		cerr << "Warning: Fitness is initialized several times." << endl;
     }
     Fitness::instance = new Fitness(param);
 }
 
 
-// functions
 
-/* get and return the mean phenotypic value of the population */
+/* Return the "Population value".
+   The meaning of the population value depends on the selection regimes:
+   * Linear or exponential selection: the population value is the mean phenotype
+   * Truncation selection: the population value is the truncation point
+   * Other selection regimes: the population value is meaningless, the function returns 0.0
+     but it could be anything. */
+     /* Note: this needs to be rethought for multidimensional phenotypes */
 double Fitness::GetPopulationValue(const Population& popul)
 {
     string type = instance->type;
@@ -84,28 +90,47 @@ double Fitness::GetPopulationValue(const Population& popul)
 }
 
 
-/* ??? */
+/* This function is called every generation and updates the selection function
+   when it is expected to change with time (e.g., fluctuating selection).
+   The generation number is used to trigger periodic changes */
 void Fitness::update_generation(const long unsigned int generation_number)
 {
+	// T is the period of fluctuations (for periodic changes)
     long int T = instance->param.getpar(FITNESS_PERIOD)->GetInt();
+    
+    // Strength of selection. The meaning of s2 depends on the details of the
+    // selection regime.
     double s1 = instance->param.getpar(FITNESS_STRENGTH)->GetDouble();
     double s2 = instance->param.getpar(FITNESS_STRENGTH2)->GetDouble();
+    
+    // Optimum of the selection. The meaning of o2 depends on the details of the
+    // selection regime.
     double o1 = instance->param.getpar(FITNESS_OPTIMUM)->GetDouble();
     double o2 = instance->param.getpar(FITNESS_OPTIMUM2)->GetDouble();
 
 	string fluct_type = instance->param.getpar(FITNESS_FLUCT)->GetString();
 	if (fluct_type == FF_nofluct) 
 	{
+		// No fluctuations: the strength and the optimum are directly set from s1 and o1.
+		// Note that s2 and o2 have no meaning (reading them from the dataset is useless).
         instance->strength = s1;
         instance->optimum = o1;		
 	} 
 	else if (fluct_type == FF_smooth) 
 	{
+		// Smooth fluctuations. The selection optimum follows a sinusoid curve alternating
+		// between o1 and o2 with a period T. The selection strength oscillates between s1 and
+		// s2 in the same way, whatever it might mean. (be careful to understand the implications
+		// of setting different s1 and s2!). 
         instance->strength = s2+(s1-s2)*(1.0+cos(2.0*generation_number*M_PI/double(T)))/2.0;
         instance->optimum = o2+(o1-o2)*(1.0+cos(2.0*generation_number*M_PI/double(T)))/2.0;		
 	} 
 	else if (fluct_type == FF_pflips) 
 	{
+		// Regular flips between two states (o1 and o2 for the optimum, s1 and s2 for
+		// the strength of selection). Every T/2 generations, the selection optimum 
+		// shifts from o1 to o2, and T/2 generations afterwards, from o2 to o1.
+		// The same for the selection strength, whatever it might mean. 
         if (int(generation_number / double(T/2)) % 2 == 0)
         {
             instance->strength = s1;
@@ -119,13 +144,20 @@ void Fitness::update_generation(const long unsigned int generation_number)
 	} 
 	else if (fluct_type == FF_sflips) 
 	{
-        // It's simpler to ignore the current values and to switch independently:
-        if (Random::randnum() < 1.0 / double(T) / 2.0)
+		// The same as for periodic flips, except that o1 / o2 flips (or s1 / s2) occur
+		// IN AVERAGE every T generations, but the generation at which it happens is not predictable. 
+        // Note that the algorithm is far from perfect: 
+        // * 1. it uses a "dirty" trick: every T/2 generations, the strength/optimum is set randomly to 
+        //      s1 - o1 or s2 - o2. Thus, half "shifts" do not shift anything, it just remains where it was. 
+        //      this allows to write an algorithm that does not need to test its current state. 
+        // * 2. when a shift occurs, it occurs for both strength and optimum at the same time. The pairs s1 - o1 and
+        //      s2 - o2 are thus never decoupled. I don't know if this is a bug or a feature. 
+        if (Random::randnum() < (1.0 / double(T) / 2.0))
         {
             instance->strength = s1;
             instance->optimum  = o1;
         }
-        if (Random::randnum() < 1.0 / double(T) / 2.0)
+        if (Random::randnum() < (1.0 / double(T) / 2.0))
         {
             instance->strength = s2;
             instance->optimum  = o2;
@@ -133,6 +165,10 @@ void Fitness::update_generation(const long unsigned int generation_number)
 	} 
 	else if (fluct_type == FF_brown) 
 	{
+		// Brownian motion. Every T generations, the strength and the optimum change, and the change is
+		// normally distributed, centered on the former value. 
+		// s2 and o2 here stand for the standard deviation of the change. They have a completely different meaning
+		// as for other fluctuation regimes. 
         if ((generation_number % T) == 0)
         {
             instance->strength = instance->strength + Random::randgauss()*s2;
@@ -141,6 +177,9 @@ void Fitness::update_generation(const long unsigned int generation_number)
 	} 
 	else if (fluct_type == FF_white) 
 	{
+		// White noise: the same spirit as for the Brownian motion, but the process has no memory. A new
+		// strength and optimum are drawn from a Gaussian distribution every T generations around a constant
+		// mean value. 
         if ((generation_number % T) == 0)
         {
             instance->strength = s1 + Random::randgauss()*s2;
@@ -154,15 +193,10 @@ void Fitness::update_generation(const long unsigned int generation_number)
 }
 
 
-/* ??? */
-void Fitness::update_extra(double strength)
-{
-    instance->strength = strength;
-    instance->type = 1; // linear directional selection
-}
-
-
-/* ???? */
+/* Computes the fitness associated with a given phenotype. A reference to the current population
+   has to be provided, because some fitness functions requires a "population value" (see Fitness::GetPopulationValue). 
+   This function simply computes this population value, and calls an overloaded version of compute that does
+   not need the full population as a reference. */
 double Fitness::compute(const Phenotype & phenotype, const Population & population)
 {
     double population_value = Fitness::GetPopulationValue(population);
@@ -170,30 +204,42 @@ double Fitness::compute(const Phenotype & phenotype, const Population & populati
 }
 
 
-/* ???? */
+/* Computes the fitness for a particular phenotype, given a "population value", which is necessary for some
+   selection regimes. 
+   So far, this function is very simple and computes fitness based on only one trait (the first phenotype). This
+   will have to change in the close future. */
 double Fitness::compute(const Phenotype & phenotype, double population_value)
 {
-	int focal_phen = 0; // This will have to change for multivariate selection
+	int focal_phen = 0; // This will have to change for multivariate selection!!!!
 	
     assert (instance != NULL);
     double fit = 0.0;
 
 	string type = instance->type;
 	if (type == FT_nosel) {
+		// No selection: every phenotype has the same fitness (1.0, but it could be any positive number)
 		fit = 1.0;
 	} else if (type == FT_linear) {
+		// Linear selection. w = 1 + s(x-µ), where s is the selection strength, and µ is the population mean. 
+		// The population mean is provided as the "population value"
+		// Note that linear selection could lead to negative values of fitness, which need to be set to 0.
         fit = 1.0 + instance->strength*(phenotype[focal_phen] - population_value);
         if (fit < 0)
         {
             fit = 0;
         }		
 	} else if (type == FT_expo) {
+		// Exponential selection. w = exp[s(x-µ)]
 		fit = exp(instance->strength*(phenotype[focal_phen] - population_value));
 	} else if (type == FT_gauss) {
+		// Gaussian selection (stabilizing around an optimum o).
+		// w = exp[-s(x-o)²]
 		fit = exp(-instance->strength*
 			(phenotype[focal_phen] - instance->optimum)*
             (phenotype[focal_phen] - instance->optimum));		
 	} else if (type == FT_quad) {
+		// Quadratic selection: w = 1 - s(x-o)².
+		// This can lead to negative fitness values that must be set to 0. 
         fit = 1.0 - instance->strength*
               (phenotype[focal_phen] - instance->optimum)*
               (phenotype[focal_phen] - instance->optimum);
@@ -202,18 +248,26 @@ double Fitness::compute(const Phenotype & phenotype, double population_value)
             fit = 0;
         }		
 	} else if (type == FT_truncup) {
+		// Up truncation selection. w = 1 if x > t, w = 0 otherwise.
+		// The threshold t is provided as the "population value".
         fit = 0.0;
         if (phenotype[focal_phen] > population_value)
             fit = 1.0;		
 	} else if (type == FT_truncdown) {
+		// Down truncation selection. w = 1 if w < t, w = 0 otherwise. 
         fit = 0.0;
         if (phenotype[focal_phen] < population_value)
             fit = 1.0;		
 	} else if (type == FT_concave) {
+		// Concave fitness function. w = 1 + log(1 + 2s(x-µ))/2
+		// The slope of this function is s when x = µ
+		// Note that this function can return negative fitnesses. 
         fit = 1.0 + 0.5*log(1.0+2.0*instance->strength*(phenotype[focal_phen] - population_value));
         if (fit < 0.0)
             fit = 0.0;		
 	} else if (type == FT_convex) {
+		// Convex fitness function. w = exp(-sqrt(s)|x-µ|)
+		// As the previous one, the slope is s when x = µ. 
         fit = exp(-sqrt(instance->strength*
             (phenotype[focal_phen] - instance->optimum)*
             (phenotype[focal_phen] - instance->optimum)));		
--- Fitness.h
+++ Fitness.h
@@ -1,5 +1,5 @@
 // Copyright 2004-2007 José Alvarez-Castro <jose.alvarez-castro@lcb.uu.se>
-// Copyright 2007      Arnaud Le Rouzic    <a.p.s.lerouzic@bio.uio.no>
+// Copyright 2007-2014 Arnaud Le Rouzic    <a.p.s.lerouzic@bio.uio.no>
 
 
 /***************************************************************************
@@ -23,26 +23,27 @@
 #include <string>
 
 
-
 class Fitness
 {
+	// This is a singleton class: only one instance, access only through static
+	// functions (no public constructor)
     public:
-        // constructors/destructor
-        Fitness(const ParameterSet&);
-
         // instance/initialization
-        static Fitness * instance;
         static void initialize(const ParameterSet&);
 
         // fonctions
         static double GetPopulationValue(const Population &);
         static void update_generation(const long unsigned int);
-        static void update_extra(double strength);
         static double compute(const Phenotype& phenotype, const Population & population);
         static double compute(const Phenotype& phenotype, double population_value);
         static double current_optimum() {return(instance->optimum);}
 
     protected:
+        // constructors/destructor
+        Fitness(const ParameterSet&);    
+    
+        static Fitness * instance;
+
         const ParameterSet & param;
         std::string type;
         double strength;
