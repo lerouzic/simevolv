24e7f170d1a20c2d742ca2e7c785ed7ce4c5253c
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2017-02-06 16:06:09 +0100

    Add new R script file, misc.R : various helper functions. So far, only one function, mcsapply, a parallel version of sapply.

---
+++ script/misc.R
@@ -0,0 +1,25 @@
+#########################################################
+# misc.R
+#
+# Set of various helper functions.
+# Don't take it seriously, nothing fancy here. 
+# 
+# Copyright Arnaud Le Rouzic / CNRS 2015
+# <lerouzic@egce.cnrs-gif.fr>
+#
+# Released under the WTFPL version 2.0
+# * No warranty *
+###########################################################
+
+mcsapply <- 
+function (X, FUN, ..., simplify = TRUE, USE.NAMES = TRUE) 
+{
+    if (!require(parallel)) mclapply <- lapply
+    FUN <- match.fun(FUN)
+    answer <- mclapply(X = X, FUN = FUN, ...)
+    if (USE.NAMES && is.character(X) && is.null(names(answer))) 
+        names(answer) <- X
+    if (!identical(simplify, FALSE) && length(answer)) 
+        simplify2array(answer, higher = (simplify == "array"))
+    else answer
+}
