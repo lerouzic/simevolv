8957aab3d20aeaad95d88be360ba1860a71b6de4
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2017-03-20 14:46:51 +0100

    Revert "Add different indexes for canalization"
    This reverts commit 4e8698744ba7b7ca0db2d7f20f3c27a55d349305.

--- script/Wcanal.R
+++ script/Wcanal.R
@@ -54,20 +54,41 @@ dev.canalization <- function(W, a=0.5, S0=rep(a, nrow(W)), ..., nb.affected.gene
 	canIndex(apply((rr-ref)^2, 1, mean))
 }
 
-genet.canalization <- function(W, ..., mutsd=0.1, exclude.0=TRUE, replicates=100) {
+canscale <- function(x) ifelse(is.na(x) | x < exp(-20), 20, -log(x))
+
+genet.canalization <- function(W, ..., which.i=1:nrow(W), which.j=1:ncol(W), mutsd=0.1, exclude.0=TRUE, replicates=100, what="mean", reference="mutant") {
 	# This is the traditional canalization measurement (robustness to mutations)
-    ref <- model.M2(W, ...)$mean    
-    
-	is.0 <- W == 0 | !exclude.0
-	stopifnot (sum(is.0) < length(W))
-	Wbox <- sample(which(!is.0), replicates, replace=TRUE)
+	# reference = 'mutant' (variance around the average mutant expression)
+	# reference = 'wt'     (variance around the original genotype)
+	#
+	# which.i and which.j specify the lines/columns that can be mutated
+	# exclude.0 : disallow mutations of the 0s
+	
+	stopifnot(nrow(W) == ncol(W))
+	n <- nrow(W)
+	stopifnot(all(which.i > 0 & which.i <= n))
+	stopifnot(all(which.j > 0 & which.j <= n))	
+	
+	mrow <- matrix(rep(1:n, n), nrow=n)
+	mcol <- t(mrow)
+	
+	avoided <- !(mrow%in%which.i) | !(mcol%in%which.j) | (W == 0 & exclude.0)
+	stopifnot (sum(avoided) < length(W))
+
+	Wbox <- if (sum(!avoided) == 1) { rep(which(!avoided), replicates)}
+			else {sample(which(!avoided), replicates, replace=TRUE)}
 	Wdev <- rnorm(replicates, 0, sd=mutsd)
 	rr <- mcsapply(1:replicates, function(i) {
 		myW <- W
 		W[Wbox[i]] <- W[Wbox[i]]+Wdev[i]
 		model.M2(W=W, ...)$mean
 	})
-    # Computes the mean square of the differences before/after disturbance
-	canIndex(apply((rr-ref)^2, 1, mean))
+
+	if (reference == "mutant") {
+		return(apply(rr, 1, var))
+	} else if (reference == "wt") {
+		ref <- model.M2(W=W, ...)[[what]]
+		return(apply(rr-ref, 1, function(x) sum(x^2)/length(x)))
+	}	
 }
 
